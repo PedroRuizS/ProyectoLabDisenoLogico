b771a52e8155ed134d85259fe9f5aa43bb7d83ff
PedroRuizS <98855093+PedroRuizS@users.noreply.github.com>
2023-11-05 02:35:10 -0600

    Commit 3 ya paso 3 pruebas de escritorio entonces ya sirbe bien

--- sensor_humedad.vhd
+++ sensor_humedad.vhd
@@ -14,70 +14,88 @@ entity sensor_humedad is
 end entity sensor_humedad;
 
 architecture comportamiento of sensor_humedad is
+
+    constant umbral_humedad: integer := 40;
+    constant media_rotacion: integer := 90;
+
     signal datos_sensor: std_logic_vector(39 downto 0);
     signal humedad: std_logic_vector(15 downto 0);
-    signal temperatura: std_logic_vector(15 downto 0);
     signal checksuma: std_logic_vector(7 downto 0);
     signal datos_validos: std_logic := '0';
 
-    signal pin_entrada_init: std_logic := '0';
-    signal checksuma_init: std_logic_vector(7 downto 0) := (others => '0');
+    signal pin_entrada_init: std_logic_vector(7 downto 0) := "00000000";
+
+    signal angulo_descanso: integer;
+
+    signal tiempo_actual: time;
+    signal clk_inicio: time := 0;
 
 begin
+
     proceso_sensor: process (clk)
     begin
         if rising_edge(clk) then
+
             if inicio_tiempo = '0' then
                 datos_sensor <= (others => '0');
             end if;
 
             datos_sensor <= pin_entrada & datos_sensor(7 downto 0);
 
-            pin_salida <= '1';
+            pin_salida <= pin_entrada;
 
-            if pin_entrada = "00000001" and datos_sensor(39) = '0' then
+            if pin_entrada = '0' and datos_sensor(39) = '0' then
+
                 humedad <= datos_sensor(38 downto 23);
-                temperatura <= datos_sensor(22 downto 7);
                 checksuma <= datos_sensor(7 downto 0);
 
-                -- **Comentario:** Inicializa las señales a valores válidos
-                pin_entrada_init <= '0';
-                checksuma_init <= (others => '0');
+                datos_validos <= '1';
 
-                if (to_integer(unsigned(checksuma)) = to_integer(unsigned(humedad)) + to_integer(unsigned(temperatura))) then
-                    datos_validos <= '1';
-                else
-                    datos_validos <= '0';
-                end if;
+            else
+                datos_validos <= '0';
+            end if;
+
+            if tiempo_actual - clk_inicio > 50 us then
+                clk_inicio <= tiempo_actual;
             end if;
         end if;
     end process proceso_sensor;
 
-    -- **Comentario:** Si la inicialización no está presente, esto causará un error de rango
     pin_entrada <= pin_entrada_init;
-    checksuma <= checksuma_init;
 
-    -- Proceso para el manejo de umbrales y control del servo
-    process
+    -- **Proceso para el manejo de umbrales y control del servo**
+    proceso_control: process (clk)
     begin
         wait until rising_edge(clk);
         if (datos_validos = '1') then
-            if (to_integer(unsigned(humedad)) > to_integer(unsigned(umbral_humedad)) and
-                to_integer(unsigned(temperatura)) > to_integer(unsigned(umbral_menor_seco)) and
-                to_integer(unsigned(temperatura)) < to_integer(unsigned(umbral_mayor_mojado)) then
-                -- **Comentario:** Activa el servo (gira medio giro, 90°)
+
+            if (to_integer(unsigned(humedad)) < umbral_humedad) then
+
+                -- **Estado activado**
                 angulo_descanso <= to_integer(unsigned(media_rotacion));
-                -- **Comentario:** Utiliza el módulo de control de servo para generar la señal PWM
                 servo_angulo <= angulo_descanso;
                 servo_pwm <= '1';  -- Activa el servo
+
             else
-                -- **Comentario:** Detiene el servo (o mantenerlo inactivo, 0°)
-                angulo_descanso <= 0;
-                -- **Comentario:** Utiliza el módulo de control de servo para generar la señal PWM
-                servo_angulo <= angulo_descanso;
-                servo_pwm <= '0';  -- Detiene el servo
+
+                -- **Estado desactivado**
+                if (servo_angulo > 0) then
+                    angulo_descanso <= 0;
+                    servo_angulo <= angulo_descanso;
+                    servo_pwm <= '0';  -- Desactiva el servo
+                end if;
+
             end if;
 
+            if (servo_angulo == 90 and humedad > 80) then
+                servo_angulo <= 0;
+                servo_pwm <= '0';
+            end if;
+
+            if (servo_angulo = 0) then
+                servo_pwm <= '0';  -- El servo está en su posición de descanso
+            end if;
         end if;
-    end process;
+    end process proceso_control;
+
 end architecture comportamiento;
